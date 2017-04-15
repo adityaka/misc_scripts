3d787f4f43ea0aec42b2a79ef37991b025053f00
aditya kapur <adityaka@live.com>
2017-04-15 12:59:25 +0530

    Made changes to logging and if files are not found we log to the console

--- hadoop_scripts/hadoop-cleaner.py
+++ hadoop_scripts/hadoop-cleaner.py
@@ -3,6 +3,15 @@ import fnmatch
 import os
 import os.path 
 import sys 
+import logging 
+
+"""
+This one cleans the mess if you have installe hadoop installation forcefully 
+to the root of the linux/unix system 
+e.g. sudo cp -rvf ./hadoop-2.9.0-SNAPSHOT/* / 
+"""
+logging.basicConfig(format="%(asctime)s,%(levelname)s,%(message)s",level=logging.DEBUG)
+logger = logging.getLogger(__file__) 
 
 def main():
     arg_parser = argparse.ArgumentParser()
@@ -10,11 +19,12 @@ def main():
     parser = arg_parser.parse_args() 
 
     if parser.hadoop_root is  None or \
-    os.path.exists(parser.hadoop_root) is not True :
+    os.path.exists(parser.hadoop_root) is False:
+        logging.error("Insufficient Arguments -hadoop-root is mandetory parameter")
         arg_parser.print_help() 
         exit(-1) 
     if os.getuid() != 0:
-        sys.stderr.writelines("You need to be root to delete the hadoop dist")
+        logging.error("You need to be root to delete the hadoop dist")
         arg_parser.print_help()
         exit(-1)
 
@@ -24,8 +34,9 @@ def main():
             file_path = os.path.join(root,f).replace(parser.hadoop_root,"")
             if os.access(file_path,os.W_OK):
                 os.remove(file_path)
-                print "deleted " + file_path
-                
+                logging.info("deleted " + file_path)
+            elif os.path.exists(file_path) is False:
+                logger.info(file_path + "doesn't exist")
 
 if __name__ == "__main__":
     main()
