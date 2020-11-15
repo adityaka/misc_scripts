5548ffece74c5ad33a1d6f5433018d5039b28e14
adityaka@live.com <adityaka@live.com>
2020-11-15 20:15:29 +0530

    restructured the repo

--- prep_and_learning/dslearn/cpp/include/Exceptions.hh
+++
@@ -1,7 +0,0 @@
-#ifndef BASIC_CPP_EXCEPTIONS_H
-#define BASIC_CPP_EXCEPTIONS_H
-#include<exception>
-
-
-
-#endif
--- prep_and_learning/dslearn/python/linkedlist/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from __future__ import absolute_import 
-from linkedlist.singlelist import SingleList
-from linkedlist.doublelist import DoubleList
-
-__all__ = [SingleList, DoubleList]
